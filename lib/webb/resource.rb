8276d2d6912320bbd455711fb92ccafe017a5d82
Emmanuel Akachukwu <emmanuelakachukwu1@gmail.com>
2024-12-03 01:52:21 +0100

    feat: create resource class for representing fetched resources

---
+++ lib/webb/resource.rb
@@ -0,0 +1,16 @@
+module Webb
+  class Resource
+    attr_reader :path, :type, :sha, :size
+
+    def initialize path:, type:, sha:, size: nil
+      @path = path
+      @type = type
+      @sha = sha
+      @size = size
+    end
+
+    def is_file?
+      type == 'blob'
+    end
+  end
+end
